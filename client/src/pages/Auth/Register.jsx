78ed5b9d7e6dd821a64cfe0ea9dab8174baec6a4
gauravdevkar27 <devkardinesh18@gmail.com>
2025-08-01 00:17:06 +0530

    fix: Update register page layout and validation

--- client/src/pages/Auth/Register.jsx
+++ client/src/pages/Auth/Register.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from './Login.module.css';
+import login from '../../assets/login.png'
+import { Button, Input } from 'antd'
+import { Link } from 'react-router-dom';
+
+
 
 function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  // const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+    console.log("register");
+  }
+
   return (
     <div>
-      <h1>Register</h1>
+      <div className={styles.login__card}>
+        <img src={login} alt=".." />
+        <h2>Register</h2>
+        <div className={styles.input__inline__wrapper}>
+          <Input
+            placeholder="First Name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)} />
+          <Input
+            placeholder="Last Name"
+            style={{ marginLeft: '10px' }}
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)} />
+
+        </div>
+        <div className={styles.input__wrapper}>
+          <Input
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div className={styles.input__wrapper}>
+          <Input.Password
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <div className={styles.input__info}>
+          Existing User? <Link to="/login">Login</Link>
+        </div>
+        <Button loading={loading} type="primary" size="large" disabled={!username || !password} onClick={handleSubmit} >Register</Button>
+      </div>
     </div>
+
   )
 }
 
 export default Register
+
 

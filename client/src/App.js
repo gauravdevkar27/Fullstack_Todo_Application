14e1d26c750af6fdd631388995393f9172265283
gauravdevkar27 <devkardinesh18@gmail.com>
2025-08-01 00:32:37 +0530

    feat(router): implement React Router for page navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React from 'react'
+import {Route, Routes} from 'react-router';
 import Landing from './pages/Landing/Landing';
-import Login from './pages/Auth/Login.jsx';
+import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 import ToDoList from './pages/ToDo/ToDoList';
 import './App.css';
-import 'antd/dist/reset.css'
+import 'antd/dist/reset.css';
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Landing/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/register' element={<Register/>}/>
-      <Route path='/to-do-list' element={<ToDoList/>}/>
+   <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/to-do-list" element={<ToDoList />} />
     </Routes>
+
   )
 }
 
